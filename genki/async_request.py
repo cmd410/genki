1a219515b9b7513e2e9527cf97ea6068dd58130b
Crystal Melting Dot <stresspassing@gmail.com>
2020-10-03 15:24:34 +0300

    AsyncRequest callbacks

--- genki/async_request.py
+++ genki/async_request.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Callable
 
 from gevent import Greenlet
 from gevent.event import AsyncResult
@@ -8,6 +8,7 @@ from .http import Response
 
 class AsyncRequest:
     def __init__(self, greenlet: Greenlet):
+        self.greenlet = greenlet
         self.async_result: AsyncResult = AsyncResult()
         greenlet.link(self.async_result)
 
@@ -15,6 +16,11 @@ class AsyncRequest:
         self.async_result.wait()
         return self.async_result.value or self.async_result.exception
 
+    def bind(self,
+             callback: Callable[[Union[Response, Exception]], None]) -> 'AsyncRequest':
+        self.greenlet.link(lambda gl: callback(gl.get()))
+        return self
+
     @property
-    def is_done(self):
+    def is_done(self) -> bool:
         return self.async_result.ready()
